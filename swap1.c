7f58869cc844edaacaa5a1039c44aba39f52460e
priyansh2309 <priyansh.chouhancse2024@indoreinstitute.com>
2025-09-01 11:31:47 +0530

    Update swap1.c

--- swap1.c
+++ swap1.c
@@ -4,8 +4,23 @@ void main(){
 	int a,b,c;
 	printf("Enter Firdt String:");
 	scamf("%d",&a);
-	printf("Enter Second String:")
+	printf("Enter Second String:");
+	scanf("%d",&b);
+	printf("Before swapping a =%d and b = %d \n",a,b);
+	swap(a,b);
+	printf("After swapping a=%d and b=%d ",a,b );
+	
 }
+void Swap(int *p, int*q){
+	int c;
+	c = *p;
+	*p = *q;
+	*q = c;
+}
+
+
+
+
 
 
 
